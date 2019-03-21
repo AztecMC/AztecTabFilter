0378d434fb1f300c19e98227c904c8bc1dbd3d16
crashdemons <crashenator+gh@gmail.com>
2019-03-21 05:49:38 -0500

    cleanup source

--- src/main/java/com/github/crashdemons/aztectabcompleter/AZTabPlugin.java
+++ src/main/java/com/github/crashdemons/aztectabcompleter/AZTabPlugin.java
@@ -17,106 +17,106 @@ import org.bukkit.plugin.java.JavaPlugin;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author crash
  */
 public class AZTabPlugin extends JavaPlugin implements Listener {
+
     //internal variables 
-    private static final int TPS = 20;
     private FilterSet filters;
-    
+
     //runtime behavior variables
     public volatile boolean loaded = false;
     private volatile boolean ready = false;
-    
 
-    
-    private boolean kickEarlyJoins=true;
-    private String kickMessage="The server is still loading - check back in a moment!";
-    
-    
+    private boolean kickEarlyJoins = true;
+    private String kickMessage = "The server is still loading - check back in a moment!";
+
     public AZTabPlugin() {
         filters = new FilterSet(this);
     }
-    
-    private void log(String s){
+
+    private void log(String s) {
         getLogger().info(s);
     }
-    
-    private void loadConfig(){
+
+    private void loadConfig() {
         saveDefaultConfig();//fails silently if config exists
         reloadConfig();
-        
+
         filters.load(getConfig());
-        
         kickEarlyJoins = getConfig().getBoolean("kick-early-joins");
         kickMessage = getConfig().getString("kick-message");
-
     }
-    
-    
+
     // Fired when plugin is disabled
     @Override
     public void onDisable() {
         log("Disabling...");
 
-        loaded=false;
+        loaded = false;
         log("Disabed.");
     }
-    
+
     @Override
     public void onLoad() {
-        log("Loading... v"+this.getDescription().getVersion());
+        log("Loading... v" + this.getDescription().getVersion());
         loadConfig();
-        loaded=true;
+        loaded = true;
         log("Loaded config.");
     }
+
     @Override
     public void onEnable() {
-        log("Enabling... v"+this.getDescription().getVersion());
+        log("Enabling... v" + this.getDescription().getVersion());
         getServer().getPluginManager().registerEvents(this, this);
-        loaded=true;
+        loaded = true;
         ready = true;
         log("Enabled.");
     }
-    
-    
-    
-    
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if(!loaded) return true;
+        if (!loaded) {
+            return true;
+        }
         if (cmd.getName().equalsIgnoreCase("aztabreload")) {
-            if(!sender.hasPermission("aztectabcompleter.reload")){ sender.sendMessage("You don't have permission to do this."); return true; }
+            if (!sender.hasPermission("aztectabcompleter.reload")) {
+                sender.sendMessage("You don't have permission to do this.");
+                return true;
+            }
             loadConfig();
             sender.sendMessage("[AZTab] Config reloaded.");
             return true;
         }
         return false;
     }
-    
-    @EventHandler(priority=EventPriority.HIGH)
-    public void onCommandSuggestion(PlayerCommandSendEvent event){
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onCommandSuggestion(PlayerCommandSendEvent event) {
         Player player = event.getPlayer();
-        if(player.hasPermission("aztectabcompleter.bypass")) return;
-        if(!ready){
+        if (player.hasPermission("aztectabcompleter.bypass")) {
+            return;
+        }
+        if (!ready) {
             event.getCommands().clear();
-        }else{
-            event.getCommands().removeIf( entry -> !filters.filter(new FilterArgs(player,entry)).isAllowed );
+        } else {
+            event.getCommands().removeIf(entry -> !filters.filter(new FilterArgs(player, entry)).isAllowed);
         }
     }
-    
-    @EventHandler(priority=EventPriority.HIGH)
-    public void onPlayerLogin(PlayerLoginEvent event){
-        if(!loaded) return;
-        if(!ready){
-            if(kickEarlyJoins){
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        if (!loaded) {
+            return;
+        }
+        if (!ready) {
+            if (kickEarlyJoins) {
                 event.setKickMessage(kickMessage);
                 event.setResult(PlayerLoginEvent.Result.KICK_OTHER);
             }
         }
     }
-    
+
 }
