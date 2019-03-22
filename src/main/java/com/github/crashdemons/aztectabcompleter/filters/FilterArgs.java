ff1f2cdf08bfc8d80d27d34b018262aa71bd537b
crashdemons <crashenator+gh@gmail.com>
2019-03-22 09:04:23 -0500

    document FilterArgs class

--- src/main/java/com/github/crashdemons/aztectabcompleter/filters/FilterArgs.java
+++ src/main/java/com/github/crashdemons/aztectabcompleter/filters/FilterArgs.java
@@ -9,16 +9,34 @@ import com.github.crashdemons.util.Pair;
 import org.bukkit.entity.Player;
 
 /**
- * Semantic-sugar for Pair syntax
+ * Defines a set of arguments passed to filters for testing.
+ * Contains the command suggestion to be filtered and the player who receives the suggestion.
+ * (Semantic-sugar for Pair syntax)
  * @author crash
  */
 public class FilterArgs extends Pair<Player,String> {
+    /**
+     * Create a new pair of arguments for filters with null values.
+     */
     public FilterArgs(){
         super(null,null);
     }
+    /**
+     * Create a new pair of arguments for filters
+     * @param destination the player who is the destination of filtering outcome
+     * @param command the command name (suggestion) being filtered
+     */
     public FilterArgs(Player destination, String command){
         super(destination,command);
     }
+    /**
+     * Gets the player the filtering is being performed for.
+     * @return the player the filtering is being performed for. 
+     */
     public Player getDestination(){ return getKey(); }
+    /**
+     * Gets the command name (suggestion) being filtered.
+     * @return the command name.
+     */
     public String getCommand(){ return getValue(); }
 }
